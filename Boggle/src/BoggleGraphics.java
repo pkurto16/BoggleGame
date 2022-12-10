415a767a3ab4ba106e2d50f0ba79defa0f072123
Peter Kurto <Peter Kurto@172.25.44.77>
2022-12-10 17:10:02 -0500

    Made exit button work

--- Boggle/src/BoggleGraphics.java
+++ Boggle/src/BoggleGraphics.java
@@ -2,7 +2,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 
-public class BoggleGraphics implements WindowListener{
+public class BoggleGraphics implements WindowListener {
 	static final int squareSize = 50;
 	static final int xOffset = 50;
 	static final int yOffset = 50;
@@ -11,7 +11,7 @@ public class BoggleGraphics implements WindowListener{
 	DiceSet d;
 	Label[] dieLabels;
 	Label guessLabel;
-	String guessLabelText="";
+	String guessLabelText = "";
 	Frame f;
 	LinkedList<ArrayList<Label>> prevLabelPaths;
 	KeyListener runnerKeyListener;
@@ -19,7 +19,7 @@ public class BoggleGraphics implements WindowListener{
 	public BoggleGraphics(KeyListener keyListener) {
 		runnerKeyListener = keyListener;
 		prevLabelPaths = new LinkedList<ArrayList<Label>>();
-		dieLabels = new Label[height*width];
+		dieLabels = new Label[height * width];
 		f = new Frame();
 	}
 
@@ -33,32 +33,34 @@ public class BoggleGraphics implements WindowListener{
 
 	private void initializeFrame() {
 		f.addKeyListener(runnerKeyListener);
-		f.setSize(xOffset * 2 + squareSize * width, yOffset * 2 + squareSize * (height+1));
+		f.addWindowListener(this);
+		f.setSize(xOffset * 2 + squareSize * width, yOffset * 2 + squareSize * (height + 1));
 		f.setBackground(Color.getHSBColor(0, 0, (float) 0.1));
 		f.setLayout(null);
 		f.setVisible(true);
 	}
 
-
 	private void makeCharacterLabel(int xCoord, int yCoord, char c) {
 		Label l = new Label();
-		
-		l.setBounds(xOffset + 1 + xCoord * squareSize, yOffset + 1 + yCoord * squareSize, squareSize - 2, squareSize - 2);
+
+		l.setBounds(xOffset + 1 + xCoord * squareSize, yOffset + 1 + yCoord * squareSize, squareSize - 2,
+				squareSize - 2);
 		l.setBackground(Color.getHSBColor(0, 0, (float) 0.42));
 		l.setText("" + c);
 		l.setFont(Font.decode("Serif-36"));
 		l.setAlignment(Label.CENTER);
 		l.setForeground(Color.WHITE);
 
-		dieLabels[xCoord*4+yCoord] = l;
+		dieLabels[xCoord * 4 + yCoord] = l;
 		f.add(l);
 	}
+
 	private void makeGuessLabel() {
 		guessLabel = new Label();
 		guessLabel.setBounds(squareSize * width - 16, yOffset * 2 + squareSize * height, 0, 0);
 		guessLabel.setText(guessLabelText);
 		guessLabel.setFont(Font.decode("Serif-36"));
-		
+
 	}
 
 	public void drawShuffledLetters(char[][] charGrid) {
@@ -70,48 +72,31 @@ public class BoggleGraphics implements WindowListener{
 		f.setLayout(null);
 		f.setVisible(true);
 	}
-	public void guessAnimation(boolean isCorrectGuess) {
-		
-	}
 
-	@Override
-	public void windowOpened(WindowEvent e) {
-		// TODO Auto-generated method stub
-		
+	public void guessAnimation(boolean isCorrectGuess) {
+
 	}
 
 	@Override
 	public void windowClosing(WindowEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void windowClosed(WindowEvent e) {
 		System.exit(0);
 	}
 
 	@Override
-	public void windowIconified(WindowEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void windowOpened(WindowEvent e) {}
 
 	@Override
-	public void windowDeiconified(WindowEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void windowClosed(WindowEvent e) {}
 
 	@Override
-	public void windowActivated(WindowEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void windowIconified(WindowEvent e) {}
 
 	@Override
-	public void windowDeactivated(WindowEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void windowDeiconified(WindowEvent e) {}
+
+	@Override
+	public void windowActivated(WindowEvent e) {}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {}
 }
